4e9a99131b58301766492670205c8e5b74b21366
Artem Kulikov <78357375+artemkulikov@users.noreply.github.com>
2021-07-07 09:40:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -80,14 +80,14 @@ var pinAnri = L.icon({
     iconUrl: 'anri-pin.svg',
 
     iconSize:     [43, 47], // size of the icon
-    iconAnchor:   [29, 45], // point of the icon which will correspond to marker's location
+    iconAnchor:   [25, 36], // point of the icon which will correspond to marker's location
     popupAnchor:  [-6, -40] // point from which the popup should open relative to the iconAnchor
 });
 var pinApg = L.icon({
     iconUrl: 'apg-pin.svg',
 
     iconSize:     [43, 47],
-    iconAnchor:   [29, 45], 
+    iconAnchor:   [29, 36], 
     popupAnchor:  [-6, -40]
 });
 
